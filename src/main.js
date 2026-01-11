b1b63e0f91fc89ca4a24f25e4a938c0622c4ad4a
sekimoto-takaharu <taka20000713@yahoo.co.jp>
2026-01-11 18:49:41 +0900

    タイトル画面作成#9

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import { TitleScene } from "./game/scenes/TitleScene";
 import { MenuScene } from "./game/scenes/MenuScene";
 import { GameScene } from "./game/scenes/GameScene";
 import { ClearScene } from "./game/scenes/ClearScene";
@@ -8,5 +9,5 @@ new Phaser.Game({
   parent: "app",
   scale: { mode: Phaser.Scale.RESIZE },
   physics: { default: "arcade", arcade: { debug: false } },
-  scene: [MenuScene, GameScene, ClearScene],
+  scene: [TitleScene, MenuScene, GameScene, ClearScene], // ★ Title を追加
 });
